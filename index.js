3e6ad60a2e35c9b2164e2a3977706dda3643d56f
shreyaskamble1612 <121151780+shreyaskamble1612@users.noreply.github.com>
2023-10-16 00:39:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,118 @@
+let drums = document.getElementsByClassName("drum");
+
+for(let i = 0; i<drums.length;i++){
+    drums[i].addEventListener("Click", (e)=>{
+        // alert("Yout pressed the button !!!1");
+
+        // console.log(this)
+        let buttonInnerHTML = e.target.innerHTML;
+
+ 
+        switch (buttonInnerHTML) {
+            case w:
+                let tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case a:
+                let tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case s:
+                let tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case d:
+                let tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case j:
+                let snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case k:
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case l:
+                let bass = new Audio("sounds/kick-bass.mp3");
+                bass.play();
+                break;
+
+            default:
+                break;
+        }
+    });  
+} 
+
+
+
+// //
+
+//detecting button press
+
+// let drums = document.getElementsByClassName("drum");
+
+//  for(let i=0;i<drums.length;i++){
+//     drums[i].addEventListener("click",(e)=>{
+//         let buttonInnerHTML = e.target.innerHTML;
+ 
+//         makesound(buttonInnerHTML);
+//         buttonanimation(buttonInnerHTML);
+//     })
+// }
+
+// //detect key press
+// document.addEventListener("keypress",function(event){
+//     // alert("Key was pressed!");
+
+//     makesound(event.key);
+//     buttonanimation(event,key);
+// }) 
+// function makesound(key){
+// c
+//     switch (key) {
+//         case "w":
+//             let tom1 = new Audio("sounds/tom-1.mp3");
+//             tom1.play();
+//             break;
+//         case "a":
+//             let tom2 = new Audio("sounds/tom-2.mp3");
+//             tom2.play();
+//             break;
+//         case "s":
+//             let tom3 = new Audio("sounds/tom-3.mp3");
+//             tom3.play();
+//             break;
+//         case "d":
+//             let tom4 = new Audio("sounds/tom-4.mp3");
+//             tom4.play();
+//             break;
+//         case "j":
+//             let snare = new Audio("sounds/snare.mp3");
+//             snare.play();
+//             break;
+//         case "k":
+//             let crash = new Audio("sounds/crash.mp3");
+//             crash.play();
+//             break;
+//         case "l":
+//             let bass = new Audio("sounds/kick-bass.mp3");
+//             bass.play();
+//             break; 
+
+//         default:
+//             break;
+//     }
+
+// }
+
+// function buttonanimation(currentkey){
+
+//     var activebutton = document.querySelector("."+currentkey);
+//     activebutton.classList.add("pressed");
+
+//     setTimeout(function(){
+//         activebutton.classList.remove("pressed");
+
+//     },100)
+// }
